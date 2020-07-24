afe075da411195919c515e77e3dc5819df3b6aa7
Roberto JR <junionascimento2001@gmail.com>
2020-07-23 22:08:50 -0300

    UserController story, router sing Up

---
+++ src/app/controllers/UserController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+class UserController {
+
+    story(req: Request, res: Response) {
+
+    }
+}
+
+export default new UserController();
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
 
+
+import UserController from './app/controllers/UserController';
+
 const routes = Router();
+
+routes.post('/users', UserController.story);
 
 export default routes;
